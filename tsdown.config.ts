7ae7e1e776c216272777e61de7f2c4c70a595662
tijn <tijn@tijn.dev>
2025-11-03 22:37:17 +0100

    upd

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,5 +3,13 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
 	entry: ['./src/main.ts'],
 	platform: 'node',
-	
+	noExternal: [
+		'@effect/platform',
+		'@effect/platform-node',
+		'@types/node',
+		'chalk',
+		'consola',
+		'dedent',
+		'effect',
+	],
 })
