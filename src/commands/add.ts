b6e122cfdfd83da02379b3cc311fe2d808bd6605
tijn <tijn@tijn.dev>
2026-03-18 23:02:05 +0100

    feat: add lib import alias

--- src/commands/add.ts
+++ src/commands/add.ts
@@ -1,5 +1,5 @@
-import { getCommand } from '../lib/package-managers'
-import { $, detectPackageManager } from '../lib/utils'
+import { getCommand } from '#lib/package-managers.ts'
+import { $, detectPackageManager } from '#lib/utils.ts'
 import { defineCommand } from 'citty'
 
 export const add = defineCommand({
